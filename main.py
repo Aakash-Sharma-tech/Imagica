d33a1030c73cb9de89226bb1f55b6a1ca7b49d38
Aakash Sharma | Contributor <as8989607974@gmail.com>
2025-12-09 14:15:04 +0530

    Modify app.run to use environment variable for port

--- main.py
+++ main.py
@@ -108,5 +108,6 @@ def process_image():
             'processed_url': img_url
         })
         
-
-app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
